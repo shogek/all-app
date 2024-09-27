333cb0edddd987db4501a320ba4b7809146ed524
shogek <benas.bagdanovas@gmail.com>
2024-09-27 22:42:56 +0300

    Deploy to gh-pages

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,10 @@
-import js from '@eslint/js';
-import mantine from 'eslint-config-mantine';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  ...mantine,
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -24,4 +22,4 @@ export default tseslint.config(
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   }
-);
+)
