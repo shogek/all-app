2227be19a650d508c1d9b565a9143e18cbde5c96
shogek <benas.bagdanovas@gmail.com>
2024-09-28 22:25:45 +0300

    Add a root page that persists between pages

---
+++ src/components/pages/root/root-page.tsx
@@ -0,0 +1,24 @@
+import HomeIcon from '@mui/icons-material/Home'
+import AppBar from '@mui/material/AppBar'
+import IconButton from '@mui/material/IconButton'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import { Link, Outlet } from 'react-router-dom'
+
+export default function RootPage() {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <IconButton size="large" color="inherit" component={Link} to="/all-app/">
+            <HomeIcon />
+          </IconButton>
+
+          <Typography variant="body1">Deployed @ 22:25</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Outlet />
+    </div>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,17 +2,25 @@ import { createBrowserRouter, RouteObject } from 'react-router-dom'
 import ChecklistNotePage from './components/pages/checklist-note/checklist-note-page'
 import ErrorPage from './components/pages/error/error-page'
 import HomePage from './components/pages/home/home.page'
+import RootPage from './components/pages/root/root-page'
 
 const routes: RouteObject[] = [
   {
     path: '/all-app/',
-    element: <HomePage />,
+    element: <RootPage />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: '/all-app/checklist-note/:checklistNoteId',
-    element: <ChecklistNotePage />,
-    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: 'checklist-note/:checklistNoteId',
+        element: <ChecklistNotePage />,
+        errorElement: <ErrorPage />,
+      },
+    ],
   },
 ]
 
